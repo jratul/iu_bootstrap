5256d9ffed593146efe648929fd6697642d42d42
Keonyeol Yu <jratul@nate.com>
2018-05-03 16:40:47 +0900

    *add design for login.html

---
+++ js/login.js
@@ -0,0 +1,9 @@
+$('document').ready(function() {
+	$('.login-btn').click(function() {
+		alert('서버를 살 돈이 없어요..');
+	});
+
+	$('.social-login-btn').click(function() {
+		alert('서버를 살 돈이 없어요..');
+	});
+});
